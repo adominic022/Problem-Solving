d883831293c9024274c1c7def702a8f446006f62
adominic022 <38525133+adominic022@users.noreply.github.com>
2020-01-02 22:16:06 +0900

    Create amicable.cpp

---
+++ Baekjoon/7588/amicable.cpp
@@ -0,0 +1,44 @@
+
+/*
+https://www.acmicpc.net/problem/7588
+Amicable numbers between 1 and 300
+220 284
+
+*/
+
+
+#include <iostream>
+int amicable(int num_1, num_2);
+
+int main()
+{
+	bool key_pause = false;
+	unsigned int inp_val;
+
+	while (key_pause == false)
+	{
+		std::cin >> inp_val;
+
+		if (inp_val	== 0)
+		{
+			key_pause = true;
+			continue;
+		}
+		else
+		{
+
+			std::cout << "Amicable	numbers betwee " << << "and " << ;
+			std::cout << val1 << " " << val2;
+		}
+
+	}
+
+    std::cout << a + b;
+
+    return 0;
+}
+
+int amicable(int num_1, num_2)
+{
+
+}
